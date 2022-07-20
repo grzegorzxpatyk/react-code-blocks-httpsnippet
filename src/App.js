17e05e2a54f746d6a208009c8fc0ed6da760f01b
Grzegorz Patyk <grzegorz.patyk@ttms.pl>
2022-07-20 12:58:34 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { CopyBlock, solarizedDark as theme } from 'react-code-blocks';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const text =
+        "const text = 'Hello, it is nice to meet you.';\nconst greeting = (text) => {\n\tconsole.log(text);\n}\ngreeting(x);";
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>Hello React!</p>
+                <div
+                    style={{
+                        fontFamily: '"JetBrains Mono", monospace',
+                    }}
+                >
+                    <CopyBlock
+                        language="javascript"
+                        text={text}
+                        codeBlock
+                        theme={theme}
+                        showLineNumbers={true}
+                    />
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
